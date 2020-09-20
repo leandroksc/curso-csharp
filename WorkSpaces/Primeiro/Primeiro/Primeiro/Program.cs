b566eca9dfc79e7e06e76a4e198644ac44aec145
Leandro <leandroksc@gmail.com>
2020-09-20 20:33:48 -0300

    Versão inicial

---
+++ WorkSpaces/Primeiro/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Primeiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            string[] a = "teste de split".Split(' ');
+
+            for (int i = 0; i < a.Length; i++) {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
